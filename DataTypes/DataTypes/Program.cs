bfa58fc8538d3ebf12f0c24d2ff119bf88c982bb
obaid1 <obaid97r@gmail.com>
2022-04-22 17:41:19 +0100

    Data Types

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int: {0}", sizeof(int));
+            Console.WriteLine("Size of long: {0}", sizeof(long));
+            Console.WriteLine("Size of float: {0}", sizeof(float));
+            Console.WriteLine("Size of double: {0}", sizeof(double));
+            Console.WriteLine("Size of decimal: {0}", sizeof(decimal));
+            Console.WriteLine("Size of boolean: {0}", sizeof(bool));
+            Console.WriteLine("Size of char: {0}", sizeof(char));
+            Console.WriteLine("Size of byte: {0}", sizeof(byte));
+            Console.ReadLine();
+
+        }
+    }
+}
