be905c13e87e5d98aed1988218a2f3c4ac191527
obaid1 <obaid97r@gmail.com>
2022-04-22 17:48:57 +0100

    Selection Statements

---
+++ Selection_Statements/Selection_Statements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Selection_Statements
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int firstNumber = 0;
+            int secondNumber = 0;
+            Console.Write("Enter first number: ");
+            firstNumber = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine("first number is larger than second number");
+                
+            }
+            else if (firstNumber == secondNumber)
+            {
+                Console.WriteLine("first number equals second number");
+            }
+            else
+            {
+                Console.WriteLine("Second number is greater than first number");
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
