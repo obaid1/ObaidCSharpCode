e268810c69232b5db61eb01ee8293056526ad2b9
obaid1 <obaid97r@gmail.com>
2022-04-22 17:47:37 +0100

    Loops

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Loops
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double sum = 0;
+            double count = 0;
+
+            
+            do
+            {
+                sum = sum + Math.Pow(count, 2.0);
+                Console.WriteLine("count: {0} sum: {1}", count, sum);
+                count++;
+            } while (count != 10);
+            
+            
+            
+            //while loop
+            while (count!= 50)
+            {
+                sum = sum + Math.Pow(count, 2.0);
+                Console.WriteLine("count: {0} sum: {1}", count, sum);
+                count++;
+            }
+            Console.WriteLine("End loop");
+
+            int i; 
+            for (i = 1; i<=100; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("End of loop");
+
+
+            //Display numbers in the array
+            int[] arr = { 2, 4, 6, 8, 10 };
+
+            foreach (int val in arr)
+            {
+                Console.WriteLine(val);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
