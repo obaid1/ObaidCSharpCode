6e5d2a1402adca04761beab1939cdfbbc5f78bb3
obaid1 <obaid97r@gmail.com>
2022-04-22 17:49:56 +0100

    Switch Cases

---
+++ Switch_Case/Switch_Case/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = 0;
+            Console.Write("Enter a positive number: ");
+
+            x = Convert.ToInt16(Console.ReadLine());
+            if (x >= 0)
+            {
+                switch (x)
+                {
+                    case 0:
+                        Console.WriteLine("The value of a is zero");
+                        break;
+                    case 1:
+                        Console.WriteLine("The value of a is one");
+                        break;
+                    case 2:
+                        Console.WriteLine("The value of a is two");
+                        break;
+                   
+                    case 3:
+                        Console.WriteLine("The value of a is three");
+                        break;
+                    case 4:
+                        Console.WriteLine("The value of a is four");
+                        break;
+                    case 5:
+                        Console.WriteLine("The value of a is five");
+                        break;
+                    default:
+                        Console.WriteLine("The value is greater than five");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Negative numbers not allowed");
+            }
+            Console.ReadLine();
+        }
+    
+
+    }
+}
