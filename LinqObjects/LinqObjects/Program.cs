3853e3bc41b1678b59689abdd63f51b18a886ead
obaid1 <obaid97r@gmail.com>
2022-04-22 17:43:13 +0100

    Linq Objects

---
+++ LinqObjects/LinqObjects/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqObjects
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string likes = "I like fruit";
+            string[] fruits = { "Orange", "Apple", "Grapefruit", "Pear", "Pineapple","Grapes", "Peach","Melon","Coconut" };
+            int[] numbers = { 5, 6, 3, 8, 2, 9, 1, 6, 15, 66, 34, 23, 32, 45, 29, 30 };
+            var getNumbers = from number
+            in numbers
+                where number % 2 == 0
+                orderby number
+                select number;
+            Console.WriteLine(string.Join(", ", getNumbers));
+            var fruitwithG = from fruit in fruits
+                             where
+                             fruit.Contains("G") && (fruit.Length <8)
+                             select fruit;
+            Console.WriteLine(string.Join(", ", fruitwithG));
+            Console.ReadLine();
+        }
+    }
+}
