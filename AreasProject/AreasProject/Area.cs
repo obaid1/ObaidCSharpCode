826c6b2974896412472f3f768b98bdd511c6cae0
obaid1 <obaid97r@gmail.com>
2022-04-26 15:27:49 +0100

    Circle Area

---
+++ AreasProject/AreasProject/Area.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreasProject
+{
+    class Area
+    {
+        public double AreaC(double r)
+        {
+            double val = Math.PI * r * r;
+            return val = Math.Round(val, 2);
+        }
+
+        public double Perimeter(double r)
+        {
+            double val1 = 2 * Math.PI * r;
+            return val1 = Math.Round(val1, 2);
+        }
+    }
+}
---
+++ AreasProject/AreasProject/AreaTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AreasProject
+{
+    [TestFixture]
+    class AreaTest
+    {
+        [Test]
+
+        public void AreaTest1()
+        {
+            Area a = new Area();
+
+            double r = 4.0;
+            double expectedValue = 50.27;
+            double actualValue = a.AreaC(r);
+
+            Assert.AreEqual(expectedValue, actualValue);
+
+        }
+
+        [Test]
+
+        public void PerimeterTest()
+        {
+            Area a = new Area();
+
+            double r = 4.0;
+            double expectedValue = 25.13;
+            double actualValue = a.Perimeter(r);
+
+            Assert.AreEqual(expectedValue, actualValue);
+
+        }
+
+    }
+}
